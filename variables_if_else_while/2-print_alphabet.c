e926375479622be007d24a291e940df7eb9a3c52
IvaneHo <10608@holbertonstudents.com>
2025-02-17 16:27:03 +0100

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase using putchar only twice.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c = 'a';
+
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+    putchar('\n');
+
+    return (0);
+}
