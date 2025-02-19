6e3350f0ad46814d440b4384f77756c3a9a9b55a
IvaneHo <10608@holbertonstudents.com>
2025-02-19 16:07:26 +0100

    ayo

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+/* Header includes prototype functions */
 
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void); /* Add this prototype */
-
-#endif /* MAIN_H */
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
