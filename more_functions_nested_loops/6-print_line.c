576d31e240921d13f9e26e0144e8f03fb72e432e
IvaneHo <10608@holbertonstudents.com>
2025-02-24 14:03:00 +0100

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - check the code
+ * @n: est un int
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	_putchar('\n');
+}
