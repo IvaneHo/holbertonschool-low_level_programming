aeeb8ede660ac6ea11033a4b349f522d75ffb16f
IvaneHo <10608@holbertonstudents.com>
2025-02-26 15:50:57 +0100

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int _atoi(char *s)
 {
     int i = 0;
     int sign = 1;
-    unsigned int result = 0; /* Utiliser un `unsigned int` pour éviter l'overflow */
+    unsigned int result = 0;
     int found_number = 0;
 
     /* Parcourir la chaîne */
@@ -25,7 +25,7 @@ int _atoi(char *s)
             result = result * 10 + (s[i] - '0');
             found_number = 1;
 
-            /* Vérifier si le résultat dépasse INT_MAX */
+            /* Vérifier si le résultat dépasse la plage d'un int */
             if (result > (unsigned int)INT_MAX + 1 && sign == -1)
                 return (INT_MIN);
             else if (result > (unsigned int)INT_MAX && sign == 1)
@@ -37,5 +37,9 @@ int _atoi(char *s)
         i++;
     }
 
-    return (sign * (int)result);
+    /* Retourner INT_MIN si nécessaire */
+    if (result == (unsigned int)INT_MAX + 1 && sign == -1)
+        return (INT_MIN);
+
+    return ((int)result * sign);
 }
