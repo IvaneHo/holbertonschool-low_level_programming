464575f3249f7ad3955ebdb03106bc46fa02d314
IvaneHo <10608@holbertonstudents.com>
2025-02-26 15:46:58 +0100

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h> /* Pour INT_MIN et INT_MAX */
 
 /**
  * _atoi - Converts a string to an integer.
@@ -10,7 +11,7 @@ int _atoi(char *s)
 {
     int i = 0;
     int sign = 1;
-    int result = 0;
+    unsigned int result = 0; /* Utiliser un `unsigned int` pour éviter l'overflow */
     int found_number = 0;
 
     /* Parcourir la chaîne */
@@ -23,13 +24,18 @@ int _atoi(char *s)
         {
             result = result * 10 + (s[i] - '0');
             found_number = 1;
+
+            /* Vérifier si le résultat dépasse INT_MAX */
+            if (result > (unsigned int)INT_MAX + 1 && sign == -1)
+                return (INT_MIN);
+            else if (result > (unsigned int)INT_MAX && sign == 1)
+                return (INT_MAX);
         }
         else if (found_number) /* Arrêter dès qu'on a trouvé un nombre */
             break;
 
-i++;
-}
+        i++;
+    }
 
-return (result * sign);
+    return (sign * (int)result);
 }
-
