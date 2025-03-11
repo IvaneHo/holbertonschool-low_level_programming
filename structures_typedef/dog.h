14dbf1d245faf1701a028d70d059858b1248301a
IvaneHo <10608@holbertonstudents.com>
2025-03-11 15:38:53 +0100

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,16 +2,16 @@
 #define DOG_H
 
 /**
- * struct dog - Structure to define a dog
+ * struct dog - Structure defining a dog
  * @name: Pointer to the dog's name
  * @age: Dog's age
  * @owner: Pointer to the owner's name
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */
