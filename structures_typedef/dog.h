b847d15563bd2f469a8398f5a6e1a823c492f65d
IvaneHo <10608@holbertonstudents.com>
2025-03-12 11:10:08 +0100

    3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,9 +16,13 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 /* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif /* DOG_H */
-
