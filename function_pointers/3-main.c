fc00ca426d8c330515962a0c210cf1832d485355
IvaneHo <10608@holbertonstudents.com>
2025-03-17 10:57:16 +0100

    functions skjdfnejkznfdikjnzf

---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Performs simple operations.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: 0 on success, exits with status codes on errors.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = operation(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
