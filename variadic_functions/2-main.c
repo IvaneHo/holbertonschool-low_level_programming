def155ec540517ebd729c98d03c60d232c98ef82
IvaneHo <10608@holbertonstudents.com>
2025-03-18 10:18:42 +0100

    variadic

--- variadic_functions/2-main.c
+++ variadic_functions/2-main.c
@@ -9,9 +9,6 @@
 int main(void)
 {
     print_strings(", ", 2, "Jay", "Django");
-    print_strings("; ", 3, "Hello", NULL, "World");
-    print_strings(NULL, 2, "Alice", "Bob");
-    print_strings(", ", 1, "Only one");
     print_strings(", ", 0);
     return (0);
 }
