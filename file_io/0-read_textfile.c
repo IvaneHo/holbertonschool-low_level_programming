9cb2370017cb06d0d03e54c6cdd0d448fc5d4605
IvaneHo <10608@holbertonstudents.com>
2025-04-03 14:59:37 +0200

    yo

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,52 +1,31 @@
 #include "io.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the
- *                 POSIX standard output.
- * @filename: Name of the file to read.
- * @letters: Number of letters to read and print.
- *
- * Return: Actual number of letters it could read and print,
- *         or 0 on failure.
- */
-
+  * read_textfile - ...
+  * @filename: The source file
+  * @letters: Number of letters to reads and prints
+  *
+  * Return: ...
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t r, w;
-	char *buffer;
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
-	if (filename == NULL)
+	if (!buff)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
 
-	r = read(fd, buffer, letters);
-	if (r == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1 || w != r)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	free(buffer);
+	free(buff);
 	close(fd);
-	return (w);
+	return (readed);
 }
