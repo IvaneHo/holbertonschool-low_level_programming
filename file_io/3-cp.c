dec4ccdff40c116ede6ccff1e5e8a5fd749e6ffa
IvaneHo <10608@holbertonstudents.com>
2025-04-05 17:31:04 +0200

    33333333333333333333333333

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,25 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
- * print_error - prints error to stderr and exits
+ * print_error - prints error message to stderr and exits
  * @code: exit code
  * @msg: format string
- * @file: filename to print
+ * @arg: file name
  */
-void print_error(int code, const char *msg, const char *file)
+void print_error(int code, const char *msg, const char *arg)
 {
-	dprintf(STDERR_FILENO, msg, file);
+	dprintf(STDERR_FILENO, msg, arg);
 	exit(code);
 }
 
 /**
- * close_file - closes fd and checks for error
+ * safe_close - closes file descriptor and handles error
  * @fd: file descriptor
  */
-void close_file(int fd)
+void safe_close(int fd)
 {
 	if (close(fd) == -1)
 	{
@@ -29,10 +26,10 @@ void close_file(int fd)
 }
 
 /**
- * main - copies a file to another file
+ * main - copies the content of one file to another
  * @ac: argument count
- * @av: argument values
- * Return: 0 on success, exit on failure
+ * @av: argument vector
+ * Return: 0 on success, exits with specific codes on error
  */
 int main(int ac, char **av)
 {
@@ -50,7 +47,7 @@ int main(int ac, char **av)
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		close_file(fd_from);
+		safe_close(fd_from);
 		print_error(99, "Error: Can't write to %s\n", av[2]);
 	}
 
@@ -59,19 +56,19 @@ int main(int ac, char **av)
 		w = write(fd_to, buffer, r);
 		if (w != r)
 		{
-			close_file(fd_from);
-			close_file(fd_to);
+			safe_close(fd_from);
+			safe_close(fd_to);
 			print_error(99, "Error: Can't write to %s\n", av[2]);
 		}
 	}
 	if (r == -1)
 	{
-		close_file(fd_from);
-		close_file(fd_to);
+		safe_close(fd_from);
+		safe_close(fd_to);
 		print_error(98, "Error: Can't read from file %s\n", av[1]);
 	}
 
-	close_file(fd_from);
-	close_file(fd_to);
+	safe_close(fd_from);
+	safe_close(fd_to);
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -10,4 +10,10 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+int main(int ac, char **av); /* pour 3-cp.c */
+
+void print_error(int code, const char *msg, const char *arg); /* si utilisée */
+void safe_close(int fd); /* ou close_file, selon ton nom */
+int _putchar(char c); /* obligatoire même si inutilisé */
+
+#endif /* MAIN_H */
